58751c19f76cb7473662d341aa7a82ac7980e2aa
optionstrader <manish@vinblitz.com>
2015-11-22 16:42:18 -0500

    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+##
+## First function creates a cache marix object 
+## second function solves the matrix for inverse 
+##
 
-## Write a short comment describing this function
+## Create a cacheMatrix object for a matrix
+## Test Run
+##      > source("cacheMatrix.R")
+## Create x
+##      > x = rbind(c(1, 2), c(2,1))
+## Make Cache Matrix
+##      > m = makeCacheMatrix(x)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  set<-function(y){
+  x<<-y
+  m<<-NULL
+}
+get<-function() x
+setmatrix<-function(solve) m<<- solve
+getmatrix<-function() m
+list(set=set, get=get,
+   setmatrix=setmatrix,
+   getmatrix=getmatrix)
 }
 
+## Make sure m is created as expected
+##      > m$get()
+##           [,1] [,2]
+##      [1,]    1    2
+##      [2,]    2    1
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cacheSolve Returns a matrix that is the inverse of 'x' created above
+## if inverse exists, gets results from cached data if not creates one
+cacheSolve <- function(x=matrix(), ...) {
+    m<-x$getmatrix()
+    if(!is.null(m)){
+      message("getting cached data")
+      return(m)
+    }
+    matrix<-x$get()
+    m<-solve(matrix, ...)
+    x$setmatrix(m)
+    m
 }
+
+## Find Inverse of the matix with cacheSolve
+##      > cacheSolve(m)
+##                 [,1]       [,2]
+##      [1,] -0.3333333  0.6666667
+##      [2,]  0.6666667 -0.3333333
+##      >
